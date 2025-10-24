0506f07fbc309afb41bbf043a5702a64d1882002
mossab arektout <mossabarektout2000@gmail.com>
2025-10-24 22:08:07 +0100

    chore: add run.py as entry point for development server

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class config:
+class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
